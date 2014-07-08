84f332ead60f8baa7cb2a245742526148dfe13ef
fvdb <frank@am-knie.de>
2014-07-08 16:14:41 +0200

    add rsync

---
+++ rsyncscript.sh
@@ -0,0 +1,8 @@
+
+rsync -rt --delete --progress /home/fvdb/halde/  \
+    /home/fvdb/zbigdata_mnt/\=BACKUP_HALDE\=
+
+rsync -rt --delete --progress /home/fvdb/dokumente/ \
+    /home/fvdb/zbigdata_mnt/\=BACKUP_DOKUMENTE\=
+
+
