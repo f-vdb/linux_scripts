3b321562e4b326f58964a99fde32f3dce2be935a
fvdb <frank@am-knie.de>
2015-07-25 16:19:01 +0200

    -a

--- rsyncscript.sh
+++
@@ -1,8 +0,0 @@
-
-rsync -rt --delete --progress /home/fvdb/halde/  \
-    /home/fvdb/zbigdata_mnt/\=BACKUP_HALDE\=
-
-rsync -rt --delete --progress /home/fvdb/docs/ \
-    /home/fvdb/zbigdata_mnt/\=BACKUP_DOKUMENTE\=
-
-
