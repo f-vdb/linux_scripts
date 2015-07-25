6eaf71cbea556a788d561cb3a2616b8e34ae2944
fvdb <frank@am-knie.de>
2015-07-25 16:13:53 +0200

    change_capslock_to_altgr

---
+++ restore_rsync.sh
@@ -0,0 +1,6 @@
+
+rsync -rt --progress /home/fvdb/zbigdata_mnt/\=BACKUP_HALDE\=   /media/truecrypt9/halde
+
+rsync -rt --progress /home/fvdb/zbigdata_mnt/\=BACKUP_EIGENE_DATEIEN\= /media/truecrypt9/eigene_dateien
+
+
---
+++ rsyncscript.sh
@@ -0,0 +1,8 @@
+
+rsync -rt --delete --progress /home/fvdb/halde/  \
+    /home/fvdb/zbigdata_mnt/\=BACKUP_HALDE\=
+
+rsync -rt --delete --progress /home/fvdb/docs/ \
+    /home/fvdb/zbigdata_mnt/\=BACKUP_DOKUMENTE\=
+
+
